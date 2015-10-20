93a2f03da9d2a3bcf60d97fc8f009b4a1650dfd6
Angus Liu (RD-TW) <angus_liu@trend.com.tw>
2015-10-20 16:28:15 +0800

    [add] collectd on el7

---
+++ monitor/deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#ansible-playbook playbook.yml -i ./hosts/dev --limit monitor-db
+#ansible-playbook playbook.yml -i ./hosts/dev --limit monitor-targets
+ansible-playbook playbook.yml -i ./hosts/dev
